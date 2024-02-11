2741f4a377f0ac807a0b59dc2486190f8cd7c4b8
appworksco <info.appworksco@gmail.com>
2024-02-11 10:55:57 +0800

    Fixed layout

--- includes/layout/dashboard-navbar.php
+++ includes/layout/dashboard-navbar.php
@@ -1,7 +1,7 @@
     <!--  Header Start -->
-    <header class="app-header w-100 d-flex align-items-center" style="height: 100px;">
+    <header class="app-header w-100" style="height: 100px;">
         <div>
-            <nav class="navbar navbar-expand-lg navbar-light">
+            <nav class="navbar navbar-expand-lg navbar-light py-1">
                 <div class="navbar-collapse px-0" id="navbarNav">
                     <div class="dropdown">
                         <button class="btn dropdown-toggle border-0 py-0" type="button" id="file" data-bs-toggle="dropdown" aria-expanded="false">
@@ -27,6 +27,21 @@
                         </ul>
                     </div>
                     <div class="dropdown">
+                        <button class="btn dropdown-toggle border-0 py-0" type="button" id="plugins" data-bs-toggle="dropdown" aria-expanded="false">
+                            Plugins
+                        </button>
+                        <ul class="dropdown-menu" aria-labelledby="plugins">
+                            <li><a class="dropdown-item py-1" href="employee">Manage Employee</a></li>
+                            <li><a class="dropdown-item py-1" href="departments">Manage Departments</a></li>
+                            <li><a class="dropdown-item py-1" href="positions">Manage Positions</a></li>
+                            <li><a class="dropdown-item py-1" href="services">Manage Services</a></li>
+                            <li><a class="dropdown-item py-1" href="issues">Manage Issues</a></li>
+                            <li><a class="dropdown-item py-1" href="municipality">Manage Municipality</a></li>
+                            <li><a class="dropdown-item py-1" href="lgu">Manage LGU</a></li>
+                            <li><a class="dropdown-item py-1" href="project-type">Manage Project Type</a></li>
+                        </ul>
+                    </div>
+                    <div class="dropdown">
                         <button class="btn dropdown-toggle border-0 py-0" type="button" id="reports" data-bs-toggle="dropdown" aria-expanded="false">
                             Reports
                         </button>
@@ -39,12 +54,12 @@
                     </div>
                 </div>
             </nav>
-            <div>
-                <button class="btn border-0">
+            <div class="border-top">
+                <button class="btn border-0 border-start border-end">
                     <img src=".././dist/icons/info.png" alt="" style="width: 40px;"> <br>
                     <small>Info</small>
                 </button>
-                <button class="btn border-0" onclick="window.location.reload()">
+                <button class="btn border-0 border-end" onclick="window.location.reload()">
                     <img src=".././dist/icons/refresh.png" alt="" style="width: 40px;"> <br>
                     <small>Refresh</small>
                 </button>
